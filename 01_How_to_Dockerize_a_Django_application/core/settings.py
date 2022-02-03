361b3b369ba0579b1bda4342538f51be11e84b76
mac_02 <mac_02@email.com>
2022-02-03 14:13:22 -0300

    Creating a Docker container

--- 01_How_to_Dockerize_a_Django_application/core/settings.py
+++ 01_How_to_Dockerize_a_Django_application/core/settings.py
@@ -19,7 +19,7 @@ from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open(os.path.join(BASE_DIR.parent, 'secrets.json')) as secrets_file:
+with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
     secrets = json.load(secrets_file)
 
 def get_secret(setting, secrets=secrets):
